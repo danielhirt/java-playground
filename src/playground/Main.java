c036117fcab2f6125d290460c561d89342c55b93
Daniel Hirt <danielchirt16@gmail.com>
2024-05-12 21:21:12 -0400

    sealed interfaces pt. 2

--- src/playground/Main.java
+++ src/playground/Main.java
@@ -1,10 +1,12 @@
 package playground;
 
+import static playground.SealedInterfaces.inspect;
 import static playground.SealedInterfaces.thisIsJustRust;
 
 public class Main {
 
     public static void main(String[] args) {
         thisIsJustRust();
+        inspect(null);
     }
 }
--- src/playground/SealedInterfaces.java
+++ src/playground/SealedInterfaces.java
@@ -2,8 +2,18 @@ package playground;
 
 import playground.SealedInterfaces.Result.Err;
 import playground.SealedInterfaces.Result.Ok;
+import playground.SealedInterfaces.WebEvent.*;
 
 class SealedInterfaces {
+
+    sealed interface WebEvent permits PageLoad, PageUnload, KeyPress, Paste, Click {
+        record PageLoad() implements WebEvent {};
+        record PageUnload() implements WebEvent {};
+        record KeyPress(char character) implements WebEvent {};
+        record Paste(String data) implements WebEvent {};
+        record Click(double x, double y) implements WebEvent {};
+    }
+
     sealed interface Result<T> permits Err, Ok {
         record Ok<T>(T value) implements Result<T> {};
         record Err<T>(Throwable err) implements Result<T> {};
@@ -24,5 +34,17 @@ class SealedInterfaces {
             case Ok<?> ok -> System.out.println(ok.value());
             case Err<?> err -> System.out.println(err.err().getMessage());
         };
+    }
+
+    static void inspect(WebEvent event) {
+        switch (event) {
+            case PageLoad ignored -> System.out.println("Page loaded!");
+            case PageUnload ignored -> System.out.println("Page unloaded!");
+            case KeyPress kp when kp.character() == 'w' -> System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+            case KeyPress kp -> System.out.println("Pressed: " + kp.character());
+            case Paste p -> System.out.println("Pasted: " + p.data());
+            case Click c -> System.out.println("Clicked at: x=" + c.x() + ", y=" + c.y());
+            case null -> System.out.println("null sadge :(");
+        }
     }
 }
